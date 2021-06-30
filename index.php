ae3baa47317e87867487fdd03dea5cba7ba029ff
ella19999 <86706900+ella19999@users.noreply.github.com>
2021-06-30 14:50:47 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+$grades =109;
+
+if ($grades<=100 && $grades <=98) 
+{ 
+    $marks= "1.00 Exelent";
+    
+}
+if ($grades<=97 && $grades <=95)
+{ 
+    $marks= "1.25 Superior";
+}
+if ($grades<=94 && $grades <=92)
+{ 
+    $marks= "1.50 Very Good";
+}
+if ($grades<=91 && $grades <=89)
+{ 
+    $marks= "1.75 Good";
+}
+if ($grades<=85 && $grades <=86)
+{ 
+    $marks= "2.00 Very Satisfactory";
+}
+if ($grades<=85 && $grades <=83)
+{ 
+    $marks= "2.25 Satisfactory";
+}
+if ($grades<=82 && $grades <=80)
+{ 
+    $marks= "2.50 Average";
+}
+if ($grades<=79 && $grades <=77)
+{ 
+    $marks= "2.75 Fair";
+}
+if ($grades<=76 && $grades <=75)
+{ 
+    $marks= "3.00 Passed";
+}
+if ($grades<=75)
+{ 
+    $marks= "Failed";
+}
+if ($grades>=101)
+{ 
+    $marks= "Wrong Input";
+}
+
+"<center>";
+echo "<h1><center>Dariela Ursulo Del Rosario</center></h1>";
+"<center>";
+echo "<h1><center>Simple Grading System</center></h1>";
+echo "<p> <u>  _________________________________________________________________________________________________________________________________________________________________________________________________________________________________________</u></p>";
+echo "<h2><center>Course Grade:$grades</center></h2>";
+echo "<h2><center>$marks</center></h2>";
+ 
+ 
+"</center>";
+?>
+
+
